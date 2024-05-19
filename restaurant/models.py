c406ef29cd93d2e0ef22cbf03758e1ba7324ac8e
Saleh <me@salehesmaeili.com>
2024-05-20 01:34:55 +0400

    Booking/Menu Models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    inventory = models.IntegerField()
+    
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    guests = models.IntegerField(default=1)
+    date = models.DateField()
